6dbc4daaf7e71c2aeed7133e5f61cc8a9d9dc0d5
Witek Bieliński <w.bielinski1@gmail.com>
2022-07-07 20:26:30 +0200

    Dodaj pliki projektów.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Informacje ogólne o zestawie zależą od poniższego 
+' zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' związane z zestawem.
+
+' Przejrzyj wartości atrybutów zestawu
+<Assembly: AssemblyTitle("Maraton_GoIT")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Maraton_GoIT")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów, jeśli ten projekt jest uwidaczniany w modelu COM
+<Assembly: Guid("d3ef8748-84cf-485f-b018-cedad8929106")>
+
+' Informacje o wersji zestawu obejmują następujące cztery wartości:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów kompilacji i poprawki, 
+' stosując znak „*”, jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
